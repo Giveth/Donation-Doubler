e8c8128fbd0ac8bb94a9d32eec20c6c2579f78e0
Griff Green <saveoursonics@gmail.com>
2017-05-05 18:57:27 -0700

    MAJOR REVISION
    Edited a lot of comments, events and function names to be stupidly readable and match the rest of giveth's `style`
    
    The only major code change was to make the fallback function feel a little safer, to remove the default setting of `amount = msg.value + this.balance` and move it to happen ONLY when `this.balance < msg.value`

--- donation_doubler.sol
+++ donation_doubler.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.10;
 
 
 /*
-    Copyright 2016, Vojtech Simetka
+    Copyright 2017, Vojtech Simetka
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,11 +18,17 @@ pragma solidity ^0.4.10;
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-/// @title Donation doubler
-/// @author Vojtech Simetka, Jordi Baylina
-/// @notice This contract is used to double a donation to a beneficiary as long as there
-/// is some money in the contract. If not, the donated value is just send directly to beneficiary
-/// with anything that may still be in this contract.
+/// @title Donation Doubler
+/// @authors Vojtech Simetka, Jordi Baylina
+/// @notice This contract is used to double a donation to a Giveth Campaign as
+///  long as there is enough ether in this contract to do it. If not, the
+///  donated value is just sent directly to designated Campaign with any ether
+///  that may still be in this contract. The `msg.sender` doubling their
+///  donation will receive twice the expected Campaign tokens and the Donor that
+///  deposited the inital funds will not receive any donation tokens. 
+///  WARNING: This contract only works for ether. A token based contract will be
+///  developed in the future. Any tokens sent to this contract will be lost.
+
 
 /// @dev `Owned` is a base level contract that assigns an `owner` that can be
 ///  later changed
@@ -97,7 +103,8 @@ contract Escapable is Owned {
     event EscapeHatchCalled(uint amount);
 }
 
-/// Just an empty contract to declare porxyPayment method
+/// @dev This is an empty contract to declare `proxyPayment()` to comply with
+///  Giveth Campaigns 
 contract Campaign {
     /// @notice `proxyPayment()` allows the caller to send ether to the Campaign and
     /// have the tokens created in an address of their choosing
@@ -106,21 +113,20 @@ contract Campaign {
     function proxyPayment(address _owner) payable returns(bool);
 }
 
-/// Main contract which doubles the amount donated.
+/// @dev Finally! The main contract which doubles the amount donated.
 contract DonationDoubler is Escapable{
-    Campaign public beneficiary;
+    Campaign public beneficiary; // expected to be a Giveth campaign
 
-    /// @notice The Constructor assigns the `escapeHatchDestination` and the
-    ///  `escapeHatchCaller`
-    ///
-    /// @param _beneficiary             The address of the vault to which a donation should be made
-    /// @param _escapeHatchDestination  The address of a safe location (usually a Multisig)
-    ///                                 to send the ether held in this contract
-    /// @param _escapeHatchCaller       The address of a trusted account or contract to
-    ///                                 call `escapeHatch()` to send the ether in this contract
-    ///                                 to the `escapeHatchDestination` it would be ideal that
-    ///                                 `escapeHatchCaller` cannot move funds out of
-    ///                                 `escapeHatchDestination`
+    /// @notice The Constructor assigns the `beneficiary`, the
+    ///  `escapeHatchDestination` and the `escapeHatchCaller` as well as deploys
+    ///  the contract to the blockchain (obviously)
+    /// @param _beneficiary The address of the Campaign controller for the Campaign
+    /// @param _escapeHatchDestination The address of a safe location (usually a
+    ///  Multisig) to send the ether held in this contract
+    /// @param _escapeHatchCaller The address of a trusted account or contract
+    ///  to call `escapeHatch()` to send the ether in this contract to the 
+    ///  `escapeHatchDestination` it would be ideal that `escapeHatchCaller`
+    ///  cannot move funds out of `escapeHatchDestination`
     function DonationDoubler(
             Campaign _beneficiary,
             address _escapeHatchCaller,
@@ -131,27 +137,31 @@ contract DonationDoubler is Escapable{
         beneficiary = _beneficiary;
     }
 
-    /// @notice Simple function to deposit more money for the doubling algorithm
-    function depositFund() payable {
-        DonationDeposit(msg.sender, msg.value);
+    /// @notice Simple function to deposit more ETH to Double Donations
+    function depositETH() payable {
+        DonationDeposited4Doubling(msg.sender, msg.value);
     }
 
-    /// @notice Donate money to the beneficiary, and if there is enough in the contract
-    ///         double it. The donor is rewarded with tokens.
+    /// @notice Donate ETH to the `beneficiary`, and if there is enough in the 
+    ///  contract double it. The `msg.sender` is rewarded with Campaign tokens
     function () payable {
-        uint amount = msg.value + this.balance;
+        uint amount;
 
-        // There is enough money in the contract to double the money sent
-        if (this.balance >= msg.value)
-            amount = msg.value * 2;
+        // When there is enough ETH in the contract to double the ETH sent
+        if (this.balance >= msg.value){
+            amount = msg.value * 2; // do it two it!
+            DonationDoubled(msg.sender, amount);
+        } else {
+            amount = msg.value + this.balance;
+            DonationSentButNotDoubled(msg.sender, amount);
+        }
 
-        // Try and send the money to the beneficiary
+        // Send the ETH to the beneficiary so that they receive Campaign tokens
         if (!beneficiary.proxyPayment.value(amount)(msg.sender))
             throw;
-
-        DonationPaid(msg.sender, amount);
     }
 
-    event DonationDeposit(address indexed sender, uint amount);
-    event DonationPaid(address indexed sender, uint amount);
+    event DonationDeposited4Doubling(address indexed sender, uint amount);
+    event DonationDoubled(address indexed sender, uint amount);
+    event DonationSentButNotDoubled(address indexed sender, uint amount);
 }
